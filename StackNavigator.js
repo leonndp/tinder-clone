e3ce99b8ab30f60371fcb651511fe016ba4e8616
Leon N. Dela Pena <leonndp@gmail.com>
2022-03-02 06:05:04 +0800

    Added LoginScreen starter

--- StackNavigator.js
+++ StackNavigator.js
@@ -3,6 +3,7 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./screens/HomeScreen";
 import ChatScreen from "./screens/ChatScreen";
+import LoginScreen from "./screens/LoginScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -11,6 +12,7 @@ const StackNavigator = () => {
     <Stack.Navigator>
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Chat" component={ChatScreen} />
+      <Stack.Screen name="Login" component={LoginScreen} />
     </Stack.Navigator>
   );
 };
