6c6d84c74f8b0635a135e4066182b61a1bf7d2eb
Leon N. Dela Pena <leonndp@gmail.com>
2022-03-02 06:04:01 +0800

    Added navigation button to chat

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,17 @@
 import { View, Text, Button } from "react-native";
 import React from "react";
+import { useNavigation } from "@react-navigation/native";
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
+
   return (
     <View>
       <Text>HomeScreen</Text>
-      <Button title="Go to Chat Screen" />
+      <Button
+        title="Go to Chat Screen"
+        onPress={() => navigation.navigate("Chat")}
+      />
     </View>
   );
 };
